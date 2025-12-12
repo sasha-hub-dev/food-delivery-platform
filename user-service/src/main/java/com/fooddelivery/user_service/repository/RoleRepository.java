abcc2b4f9f316aeb0cc530c3d6610ed1bcb429e7
sasha-hub-dev <sashabuden2007@gmail.com>
2025-12-12 23:48:39 +0300

    Implemented Repositories, Security Config, and Role Initializer)

---
+++ user-service/src/main/java/com/fooddelivery/user_service/repository/RoleRepository.java
@@ -0,0 +1,9 @@
+package com.fooddelivery.user_service.repository;
+
+import com.fooddelivery.user_service.model.Role;
+import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.Optional;
+
+public interface RoleRepository extends JpaRepository<Role, Long> {
+    Optional<Role> findByName(String name);
+}
---
+++ user-service/src/main/java/com/fooddelivery/user_service/repository/UserRepository.java
@@ -0,0 +1,10 @@
+package com.fooddelivery.user_service.repository;
+
+import com.fooddelivery.user_service.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByEmail(String email);
+    Boolean existsByEmail(String email);
+}
